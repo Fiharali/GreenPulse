418bc90ee0fd821916afd30976f9f6a3bd14af96
ali <fiharali0@gmail.com>
2024-08-30 16:17:39 +0200

    add views file

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,14 @@
-import views.Carbon;
-import services.CarbonManagement;
-import services.UserManagement;
-import views.Menu;
-import views.User;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
+import views.Menu;
 import java.util.Scanner;
 
-import static services.UserManagement.users;
 
 
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-
         Menu.mainMenu(scanner);
-
         scanner.close();
 
     }
